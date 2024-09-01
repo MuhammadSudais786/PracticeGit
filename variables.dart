0f48b41ad9f9e52479f2fe4f7174c9ae93253211
MuhammadSudais786 <sumalani830@gmail.com>
2024-09-01 12:27:41 +0500

    About var vraible

--- variables.dart
+++ variables.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  var number = 3;
+  var number = 3;   //var is a datatype which is used to store different types of values 
   var name = 'sudais';
   print('$name $number');
 }
